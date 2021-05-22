2239fad251be049bf10419302eaf11449b838e53
Vanessa Gallic <vgallic@hotmail.com>
2021-05-22 00:06:25 -0700

    Update scripts.js
    Added comments of what JS code is doing.

--- scripts.js
+++ scripts.js
@@ -2,21 +2,25 @@
 
 // Adapted code from https://www.w3schools.com/graphics/game_score.asp
 
+// Declare variables
 var myGamePiece;
 var myObstacles = [];
 var myScore;
 var answer;
 var i;
 
-document.body.style.backgroundColor="DodgerBlue";
+// Change background color of game.html.
+document.body.style.backgroundColor = "DodgerBlue";
 
+// Adding components to the game (ie.airplane and score).
 function startGame() {
+    // Controls the size and position of the components. Makes game piece into an image.
     myGamePiece = new component(100, 30, "Pictures/green airplane.png", 500, 140, "image");
     // Image by Clker-Free-Vector-Images https://pixabay.com/vectors/airplane-plane-transportation-303563/
     myScore = new component("30px", "Consolas", "black", 120, 50, "text");
     myGameArea.start();
 }
-
+// Creating a gaming area and making it ready for drawing.
 var myGameArea = {
     canvas: document.createElement("canvas"),
     start: function () {
@@ -25,13 +29,16 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.frameNo = 0;
+        // Game Area updates the display 50 times per second like movie frames.
         this.interval = setInterval(updateGameArea, 20);
     },
     clear: function () {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
+    // Clears interval and stops GameArea.
     stop: function () {
         clearInterval(this.interval);
+        // Asks user if they would like to play the game again or not. Has an error message if the user's input isn't Yes or No.
         answer = prompt("GAME OVER. Would you like to play again? Please write Yes or No.");
         var askAgain = true;
         while (askAgain == true) {
@@ -49,9 +56,8 @@ var myGameArea = {
                     answer = prompt("GAME OVER. Would you like to play again? Please write Yes or No.");
             }
         }
-    }
-}
-
+    },
+};
 function component(width, height, color, x, y, type) {
     this.type = type;
     if (type == "image") {
@@ -67,7 +73,9 @@ function component(width, height, color, x, y, type) {
     this.update = function () {
         ctx = myGameArea.context;
         if (type == "image") {
+            // Draws image of airplane.
             ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            // Writes score on canvas.
         } else if (this.type == "text") {
             ctx.font = this.width + " " + this.height;
             ctx.fillStyle = color;
@@ -77,11 +85,13 @@ function component(width, height, color, x, y, type) {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
-    }
+    };
+    // Uses speedX and speedY to change the component's position. Called before drawing the component.
     this.newPos = function () {
         this.x += this.speedX;
         this.y += this.speedY;
-    }
+    };
+    // Checks to see if component crashes with an obstacle.
     this.crashWith = function (otherobj) {
         var myleft = this.x;
         var myright = this.x + this.width;
@@ -96,11 +106,12 @@ function component(width, height, color, x, y, type) {
             crash = false;
         }
         return crash;
-    }
+    };
 }
-
+// The updateGameArea() function calls clear() and update() methods which causes the component to be drawn and cleared 50 times per second.
 function updateGameArea() {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
+    // Loops through obstacles to see if there is a crash. If there is a crash then the updateGameArea function will stop and no more drawing is done.
     for (i = 0; i < myObstacles.length; i += 1) {
         if (myGamePiece.crashWith(myObstacles[i])) {
             myGameArea.stop();
@@ -108,9 +119,12 @@ function updateGameArea() {
         }
     }
     myGameArea.clear();
+    // Counts the score.
     myGameArea.frameNo += 1;
+    // Counts frames and adds obstacles for every 150th frame.
     if (myGameArea.frameNo == 1 || everyinterval(150)) {
         x = myGameArea.canvas.width;
+        // Makes obstacles different heights inbetween 60 and 250. Gaps inbetween are always 60.
         minHeight = 60;
         maxHeight = 250;
         height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
@@ -121,6 +135,7 @@ function updateGameArea() {
         myObstacles.push(new component(30, x - height - gap, "grey", x, height + gap));
     }
     for (i = 0; i < myObstacles.length; i += 1) {
+        // Moves obstacles.
         myObstacles[i].speedX = -2;
         myObstacles[i].newPos();
         myObstacles[i].update();
@@ -130,30 +145,26 @@ function updateGameArea() {
     myGamePiece.newPos();
     myGamePiece.update();
 }
-
 function everyinterval(n) {
     if ((myGameArea.frameNo / n) % 1 == 0) {
         return true;
     }
     return false;
 }
-
+// Functions for each button to move the component in the selected direction. speedX and speedY are speed indicators.
 function moveup() {
     myGamePiece.speedY = -2;
 }
-
 function movedown() {
     myGamePiece.speedY = 2;
 }
-
 function moveleft() {
     myGamePiece.speedX = -2;
 }
-
 function moveright() {
     myGamePiece.speedX = 2;
 }
-
+  // Clears all moves from the component so it doesn't leave a trail.
 function clearmove() {
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
