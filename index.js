cac60534cac25c9dc5719a7015d54846746bd07b
Vanessa Gallic <vgallic@hotmail.com>
2021-05-22 00:05:16 -0700

    Update index.js
    Adding descriptions of what JS code is doing.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // Adapted code by Dominic Valenciana from https://stackoverflow.com/questions/39861769/ask-the-user-its-name and code by Karan from https://stackoverflow.com/questions/47965648/load-html-before-asking-for-users-input-using-prompt
 
+// Declare variables
 var delayInMilliseconds = 1000;
 var name;
+
+// Loads the html page before JS prompt and asks user for name and if input isn't a name then an error message is shown.
 
 setTimeout(function () {
     function ask() {
